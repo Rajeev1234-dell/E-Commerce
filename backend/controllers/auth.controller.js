170041bdc97273f6c0645344fd5545553a86f691
Rajeev <pmdbrajeev1@gmail.com>
2024-11-12 13:19:14 +0530

    Add pre save function to schema

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,11 +2,11 @@ import { hashPassword } from "../utils/hashPassword.js";
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
-  const pass = await hashPassword(password);
-  console.log(pass);
+  // const pass = await hashPassword(password);
+  // console.log(pass);
 
-  res.json({ name: name, email: email, password: pass });
-  // res.send("Signup route");
+  // res.json({ name: name, email: email, password: pass });
+  res.send("Signup route");
 };
 export const login = async (req, res) => {
   res.send("Login route");
