0297be0ead8e7a49f1c9cda8bef4052c709d433e
Rajeev <pmdbrajeev1@gmail.com>
2024-11-12 10:28:53 +0530

    Auth Route Setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,15 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup");
-});
-router.get("/login", (req, res) => {
-  res.send("login");
-});
-router.get("/logout", (req, res) => {
-  res.send("logout");
-});
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 
 export default router;
