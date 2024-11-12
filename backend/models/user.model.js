1801db3b5da6ef473874e6ceb7cd739d39530f99
Rajeev <pmdbrajeev1@gmail.com>
2024-11-12 13:32:48 +0530

    Added comparePassword functionality

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -39,4 +39,8 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 export default User;
